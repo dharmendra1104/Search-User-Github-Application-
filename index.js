c31a00e4b4f19ac8c46e63e268f00b6873b14ce3
dharmendra1104 <dharmendra11042@gmail.com>
2024-03-26 00:40:34 -0800

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function fetchData(url) {
     const reposResponse = await fetch(data.repos_url);
     const reposData = await reposResponse.json();
 
-    const repositoriesHTML = reposData.map(repo => `
+    const repositories = reposData.map(repo => `
       <div>
         <h>Repository ID: ${repo.id}</h><br>
         <h>Repository Name: ${repo.name}</h><br>
@@ -26,27 +26,28 @@ async function fetchData(url) {
 
     const userInfoHTML = `
       <div class="card">
+      <div class="image">
+      <img src=${data.avatar_url} alt="#"/>
+      </div>
         <div class="box">
           <div class="box-1">
-            <img src=${data.avatar_url} alt=""/>
             <p>${data.name}</p>
             <p>Git-ID : ${data.id}</p>
             <p>Location : ${data.location}</p>
             <p>Email : ${data.email}</p>
           </div>
           <div class="box-2">
-            <h4>Followers</h4>
-            <p>${data.followers}</p>
-            <h4>Following</h4>
-            <p>${data.following}</p>
-            <h4>Total Repository</h4>
-            <p>${data.public_repos}</p>
-            <a href=${data.html_url}>Github Profile</a>
+            <p>Followers : <span>${data.followers}</span></p>
+            <p>Following : <span>${data.following}</span></p>
+            <p>Total Repository : <span>${data.public_repos}</span></p>
           </div>
-        </div>
+          </div>
+          <div class="profile">
+          <a href=${data.html_url}>Github Profile</a>
+          </div>
         <div class="repos">
           <h2>Some Repositories On GitHub</h2>
-          <div class="repo1">${repositoriesHTML}</div>
+          <div class="repo1">${repositories}</div>
         </div>
       </div> `
 
